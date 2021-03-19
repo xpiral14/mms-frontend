2b619da815da2b3560f7571d2783f02e94ee73da
Erick Cesar <e.cesar88@gmail.com>
2021-03-19 00:21:31 -0400

    Added Costumer model

---
+++ src/Contracts/Models/Costumer.ts
@@ -0,0 +1,13 @@
+type Costumer = {
+  id: number;
+  companyId: number;
+  registeredBy: string;
+  updatedBy?: string;
+  name: string,
+  email?: string,
+  phone: string,
+  cnpj?: string,
+  cpf?: string
+}
+
+export default Costumer

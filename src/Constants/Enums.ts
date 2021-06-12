84192270485fda11e64e3d1f00851bd37fb8d79d
Erick Cesar <e.cesar88@gmail.com>
2021-06-12 01:37:01 -0800

    Add ScreenStatus enum

---
+++ src/Constants/Enums.ts
@@ -0,0 +1,20 @@
+export enum fuelType {
+  GASOLINE = 'Gasolina',
+  DIESEL = 'Diesel',
+  ETHANOL = 'Etanol',
+  FLEX = 'Flex',
+}
+
+export enum orderStatus {
+  PENDING = 'Aberto',
+  EXECUTING = 'Executando',
+  EXECUTED = 'Executada',
+  CANCELED = 'Cancelada',
+  DELETED = 'Apagada',
+}
+
+export enum ScreenStatus {
+  VISUALIZE = 'visualize',
+  NEW = 'new',
+  EDIT = 'edit',
+}

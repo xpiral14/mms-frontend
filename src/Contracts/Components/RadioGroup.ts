aef5680cd6f8ce22823deb4ece4c244d3578cb07
Samuel Reis <sa.reis1999@gmail.com>
2021-06-16 20:17:11 -0300

    Added prop value in RadioGroup component

--- src/Contracts/Components/RadioGroup.ts
+++ src/Contracts/Components/RadioGroup.ts
@@ -1,5 +1,6 @@
 import { RadioGroupProps as BlueprintRadioGroupProps } from '@blueprintjs/core'
 export interface RadioGroupProps extends BlueprintRadioGroupProps {
+  value?: string
   radios: {
     label?: string
     value?: string

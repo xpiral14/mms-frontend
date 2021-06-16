b9cb676c5278bda6eda7622cba2928fa3d5ae174
Samuel Reis <sa.reis1999@gmail.com>
2021-06-16 20:19:04 -0300

    Added costumer service methods

--- src/Services/CostumerService.ts
+++ src/Services/CostumerService.ts
@@ -3,17 +3,26 @@ import Costumer from '../Contracts/Models/Costumer'
 import Paginated from '../Contracts/Models/Paginated'
 
 export default class CostumerService {
-  // static DEFAULT_PATH = 
-  static async create(costumerData: Costumer) {
+  // static DEFAULT_PATH =
+  static async create(costumerData: Partial<Costumer>) {
     return api.post('/company/costumer', costumerData)
   }
-  static async getAll(page = 10, limit = 20, query?: Costumer) {
-    return api.get<Paginated<Costumer>>('/company/costumer', {
+
+  static async edit(costumerData: Partial<Costumer>) {
+    return api.put(`/company/costumer/${costumerData.id}`, costumerData)
+  }
+
+  static async getAll(page = 10, limit = 20, query?: Partial<Costumer>) {
+    return api.get<Paginated<Partial<Costumer>>>('/company/costumer', {
       params: {
         page,
         limit,
-        ...query
+        ...query,
       },
     })
+  }
+
+  static async delete(costumerId: number) {
+    return api.delete(`/company/costumer/${costumerId}`)
   }
 }

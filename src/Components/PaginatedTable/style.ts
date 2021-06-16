76fe98736faceaebe1db598753d198cdedc2d2d8
Samuel Reis <sa.reis1999@gmail.com>
2021-06-16 20:15:42 -0300

    Added Table styles

--- src/Components/PaginatedTable/style.ts
+++ src/Components/PaginatedTable/style.ts
@@ -5,15 +5,20 @@ export const Container = styled.div`
     max-height: ${(p) => p.style?.maxHeight};
     height: ${(p) => p.style?.height};
     overflow-y: ${(p) => p.style?.overflowY};
+    width: ${(p) => p.style?.width};
   }
 `
 
-export const Body = styled.section``
+export const Body = styled.section`
+  width: 100%;
+`
 
 export const Footer = styled.section`
   margin-top: 5px;
   display: flex;
   justify-content: space-between;
+  flex-direction: row-reverse;
+  width: 100%;
   ul {
     list-style-type: none !important;
     margin: 0;

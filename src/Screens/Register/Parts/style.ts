5564781b26865116bdfa534d2b35ffff1f9c7af5
Erick Cesar <e.cesar88@gmail.com>
2021-06-17 20:50:24 -0800

    Done layout

--- src/Screens/Register/Parts/style.ts
+++ src/Screens/Register/Parts/style.ts
@@ -1,21 +1,23 @@
-import styled, { CSSProperties } from 'styled-components'
+import styled from 'styled-components'
 
 export const Container = styled.main``
 
 export const Header = styled.div`
   padding-bottom: 5px;
 `
-export const Body = styled.div``
+export const Body = styled.div`
+  .flexRow {
+    display: flex;
+    gap: 5px;
+  }
 
-export const styles: { [key: string]: CSSProperties } = {
-  flexRow: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    gap: '10px',
-  },
-  tableRow: {
-    display: 'flex',
-    width: '100%',
-    minHeight: '300px',
-  },
-}
+  .w100 {
+    width: 100%;
+  }
+
+  tablerow: {
+    display: flex;
+    width: 100%;
+    min-height: 300px;
+  }
+`

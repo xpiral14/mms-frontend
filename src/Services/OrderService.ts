03bc1e0d5f2ebd8e8f492ee046733df270f9d280
Samuel Reis <sa.reis1999@gmail.com>
2021-06-19 00:16:08 -0300

    Added services

--- src/Services/OrderService.ts
+++ src/Services/OrderService.ts
@@ -1,15 +1,21 @@
 import api from '../Config/api'
-import Metadata from '../Contracts/Models/Metadata'
 import Order from '../Contracts/Models/Order'
+import Paginated from '../Contracts/Models/Paginated'
 
 export default class OrderService {
   static DEFAULT_PATH = '/company/order'
 
-  static async getAll() {
-    return api.get<Metadata<Order[]>>(this.DEFAULT_PATH)
+  static async getAll(page: number, limit: number, query: object) {
+    return api.get<Paginated<Order>>('/company/order', {
+      params: {
+        page,
+        limit,
+        ...query,
+      },
+    })
   }
 
-  static async getOne(orderId:number){
+  static async getOne(orderId: number) {
     return api.get<Order>(`${this.DEFAULT_PATH}/${orderId}`)
   }
 }

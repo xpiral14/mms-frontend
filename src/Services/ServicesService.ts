fd353190acf396c4d3789282fe3e85b4f832a250
Erick Cesar <e.cesar88@gmail.com>
2021-07-07 22:04:09 -0400

    Add DEFAULT_PATH

--- src/Services/ServicesService.ts
+++ src/Services/ServicesService.ts
@@ -2,29 +2,31 @@ import api from '../Config/api'
 import Service from '../Contracts/Models/Service'
 import Paginated from '../Contracts/Models/Paginated'
 
+const DEFAULT_PATH = '/service'
+
 class ServicesService {
   static async getAll(page = 10, limit = 20, perPage = 50) {
-    return api.get<Paginated<Service>>('/company/service', {
+    return api.get<Paginated<Service>>(`${DEFAULT_PATH}`, {
       params: {
         page,
         limit,
-        perPage
+        perPage,
       },
     })
   }
 
   static async create(pieceData: Partial<Service>) {
-    return api.post('/company/service', pieceData)
+    return api.post(`${DEFAULT_PATH}`, pieceData)
   }
 
   static async update(serviceId: number, serviceData: Service) {
-    return api.put(`/company/service/${serviceId}`, {
+    return api.put(`${DEFAULT_PATH}/${serviceId}`, {
       pieceData: serviceData,
     })
   }
 
   static async delete(serviceId: number) {
-    return api.delete(`/company/service/${serviceId}`)
+    return api.delete(`${DEFAULT_PATH}/${serviceId}`)
   }
 }
 

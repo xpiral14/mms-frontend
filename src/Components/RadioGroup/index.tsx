8468e02be2e2635f62b6706dc005c8594720ae8f
xpiral14 <sa.reis1999@gmail.com>
2021-08-01 19:15:32 -0300

    Added container component at RadioGroup

--- src/Components/RadioGroup/index.tsx
+++ src/Components/RadioGroup/index.tsx
@@ -1,14 +1,16 @@
 import { Radio, RadioGroup as BluePrintRadioGroup } from '@blueprintjs/core'
 import React from 'react'
 import { RadioGroupProps } from '../../Contracts/Components/RadioGroup'
-
+import { Container } from './style'
 const RadioGroup = React.forwardRef<any, RadioGroupProps>((props, ref) => {
   return (
-    <BluePrintRadioGroup {...props} ref={ref}>
-      {props.radios?.map((radio) => (
-        <Radio key={radio.id} {...radio} />
-      ))}
-    </BluePrintRadioGroup>
+    <Container>
+      <BluePrintRadioGroup {...props} ref={ref}>
+        {props.radios?.map((radio) => (
+          <Radio key={radio.id} {...radio} />
+        ))}
+      </BluePrintRadioGroup>
+    </Container>
   )
 })
 
---
+++ src/Components/RadioGroup/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  .bp3-control:last-child {
+    margin-bottom: 0px !important;
+  }
+`

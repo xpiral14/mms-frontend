289597f31c17878109522c48ac03d430bb2fd2dd
xpiral14 <sa.reis1999@gmail.com>
2022-04-03 11:09:05 -0300

    Fixed some api routez

--- src/Services/ServiceService.ts
+++ src/Services/ServiceService.ts
@@ -2,7 +2,7 @@ import api from '../Config/api'
 
 export default class {
   static async getAll(page = 1, perPage = 20) {
-    return api.get('/service', {
+    return api.get('/services/paginated', {
       params: {
         page,
         perPage,

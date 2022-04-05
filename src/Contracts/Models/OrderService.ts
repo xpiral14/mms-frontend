d943fff71c7d67dd47f0780e8885f3dd6863d943
xpiral14 <sa.reis1999@gmail.com>
2022-04-05 14:47:38 -0300

    Fixed attributes

--- src/Contracts/Models/OrderService.ts
+++ src/Contracts/Models/OrderService.ts
@@ -1,8 +1,8 @@
 export default interface OrderService {
-  id: number,
+  id?: number,
   order_id: number,
   service_id: number,
   quantity: number,
   description?: string,
-  replaced_price: number
+  replaced_price?: number
 }

14987162025cba80608ae06f59da9a726b5274db
xpiral14 <sa.reis1999@gmail.com>
2022-04-05 00:47:05 -0300

    Added some Layout components

---
+++ src/Components/Layout/Bar/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Bar = styled.section`
+  background-color: #ebf1f5;
+  padding: 5px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
+
+export default Bar
---
+++ src/Components/Layout/Box/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Box = styled.section`
+  box-shadow: 1px 1px 10px #0002;
+  padding: 5px;
+  border-radius: 3px;
+  font-size: 0.75rem;
+`
+
+export default Box

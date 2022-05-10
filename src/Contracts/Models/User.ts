ea5f1f3159f1d3293a24c060ac73b8c3b70e0c71
Samuel Reis <sa.reis1999@gmail.com>
2022-05-10 20:10:28 -0300

    feat: added some attributes

--- src/Contracts/Models/User.ts
+++ src/Contracts/Models/User.ts
@@ -1,8 +1,13 @@
+import { PersonType } from '../../Constants/Enums'
+
 type User = {
   id: number
   company_id: number
-  name: string
-  email: string
+  name?: string
+  email?: string
+  phone?: string
+  personType: PersonType
+  identification: string
 }
 
 export default User

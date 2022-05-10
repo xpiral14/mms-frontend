cec2e6ff48bcb3bd2c75063282b7a5ecc37c7890
Samuel Reis <sa.reis1999@gmail.com>
2022-05-10 20:14:09 -0300

    feat: added update method

--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -1,8 +1,10 @@
 import api from '../Config/api'
+import Response from '../Contracts/Models/Response'
 import User from '../Contracts/Models/User'
+import makeURL from '../Util/makeURL'
 
-export default class CompanyService {
-  static defaultPath = '/user'
+export default class UserService {
+  static defaultPath = '/users'
   static async getAll() {
     const response = api.get<User[]>(this.defaultPath)
 
@@ -12,5 +14,9 @@ export default class CompanyService {
   static async getOne(userId: number) {
     const response = api.get<User>(`${this.defaultPath}/${userId}`)
     return (await response).data
+  }
+
+  static async update(userId: number, userData: User){
+    return api.put<Response<User>>(makeURL(this.defaultPath, userId), userData)
   }
 }

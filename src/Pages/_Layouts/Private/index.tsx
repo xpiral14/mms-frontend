2b4d2eeb423da98f88d8f6616a094f570a0187c9
Samuel Reis <sa.reis1999@gmail.com>
2022-05-10 20:11:37 -0300

    feat: Added app version at private layout

--- src/Pages/_Layouts/Private/index.tsx
+++ src/Pages/_Layouts/Private/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import AppVersion from '../../../Containers/AppVersion'
 import NavBar from '../../../Containers/NavBar'
 import menu from '../../../Statics/menu'
 
-const PrivateLayout: React.FC = ({children}) => {
+const PrivateLayout: React.FC = ({ children }) => {
   return (
-    <div>
-      <NavBar menuItems={menu as any}/>
+    <div style={{ position: 'relative' }}>
+      <NavBar menuItems={menu as any} />
       {children}
+
+      <AppVersion />
     </div>
   )
 }

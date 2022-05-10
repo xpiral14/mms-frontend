5e8a33e2d50c121e76c407c1c518d0701cfc3181
Samuel Reis <sa.reis1999@gmail.com>
2022-05-10 00:24:27 -0300

    Added fake app version

---
+++ src/Containers/AppVersion/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Container } from './styles'
+
+const AppVersion = () => {
+  return <Container>1.0.0</Container>
+}
+
+export default AppVersion
---
+++ src/Containers/AppVersion/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  position: fixed;
+  right: 10px;
+  bottom: 10px ;
+
+
+`

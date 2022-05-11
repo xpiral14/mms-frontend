207f16ef2df0cb77cf34fa4c43870d88a804b069
Samuel Reis <sa.reis1999@gmail.com>
2022-05-10 23:15:59 -0300

    refactor: formatted code

--- src/Components/RadioGroup/index.tsx
+++ src/Components/RadioGroup/index.tsx
@@ -2,16 +2,26 @@ import { Radio, RadioGroup as BluePrintRadioGroup } from '@blueprintjs/core'
 import React from 'react'
 import { RadioGroupProps } from '../../Contracts/Components/RadioGroup'
 import { Container } from './style'
-const RadioGroup = React.forwardRef<any, RadioGroupProps>((props, ref) => {
-  return (
-    <Container>
-      <BluePrintRadioGroup {...props} ref={ref}>
-        {props.radios?.map((radio) => (
-          <Radio key={radio.id} {...radio} onClick={(evt) => props.onClick?.(radio.value!, evt as any)} />
-        ))}
-      </BluePrintRadioGroup>
-    </Container>
-  )
-})
+const RadioGroup = React.forwardRef<any, RadioGroupProps>(
+  ({ onClick, ...props }, ref) => {
+    return (
+      <Container>
+        <BluePrintRadioGroup {...props} ref={ref}>
+          {props.radios?.map((radio) => (
+            <Radio
+              key={radio.id}
+              {...radio}
+              onClick={
+                onClick
+                  ? (evt) => onClick?.(radio.value!, evt as any)
+                  : undefined
+              }
+            />
+          ))}
+        </BluePrintRadioGroup>
+      </Container>
+    )
+  }
+)
 
 export default RadioGroup

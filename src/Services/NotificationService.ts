3e1b33637090ce2dc05432af4a0a355d3ffbaa25
Samuel Reis <sa.reis1999@gmail.com>
2022-05-12 14:20:22 -0300

    Added service methods

--- src/Services/NotificationService.ts
+++ src/Services/NotificationService.ts
@@ -1,14 +1,19 @@
 import api from '../Config/api'
 import Notification from '../Contracts/Models/Notification'
+import Paginated from '../Contracts/Models/Paginated'
 import makeUrl from '../Util/makeURL'
 export default class NotificationService {
-  public static delete(userId: number, notificationId: number) {
-    return api.delete(makeUrl('users', userId, 'notifications', notificationId))
+  public static delete(notificationId: string) {
+    return api.delete(makeUrl('users', 'notifications', notificationId))
   }
-  public static markAsRead(userId: number, notification: Notification) {
-    return api.put(makeUrl('users', userId, 'notifications', notification.id), {
-      ...notification,
-      readed: true,
+
+  public static markAsRead(notificationId: string) {
+    return api.put(makeUrl('users','notifications', notificationId, 'read'))
+  }
+
+  public static getAll(page = 0, limit = 20) {
+    return api.get<Paginated<Notification>>('users/notifications/paginated', {
+      params: { limit, page: page  },
     })
   }
 }

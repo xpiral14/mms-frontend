7ebf06a18ce83edabdaec6a5f25bcd9247c2f35a
Samuel Reis <sa.reis1999@gmail.com>
2022-08-15 15:40:55 -0300

    feat: added openDialog when warning is fired

--- src/Pages/_Layouts/Private/index.tsx
+++ src/Pages/_Layouts/Private/index.tsx
@@ -3,19 +3,24 @@ import AppVersion from '../../../Containers/AppVersion'
 import NavBar from '../../../Containers/NavBar'
 import Notification from '../../../Contracts/Models/Notification'
 import { useAuth } from '../../../Hooks/useAuth'
+import { useDialog } from '../../../Hooks/useDialog'
 import useSocket from '../../../Hooks/useSocket'
 import { useToast } from '../../../Hooks/useToast'
 import NotificationService from '../../../Services/NotificationService'
 import menu from '../../../Statics/menu'
 
 const PrivateLayout: React.FC = ({ children }) => {
-  const { showPrimaryToast, showErrorToast, showWarningToast } = useToast()
+  const { showPrimaryToast, showErrorToast } = useToast()
   const { auth } = useAuth()
   const socket = useSocket()
+
+  const { openDialog } = useDialog()
+
   useEffect(() => {
     if (!auth) {
       return
     }
+
     const channel = socket?.private('User.' + auth.user.id)
 
     channel
@@ -40,7 +45,8 @@ const PrivateLayout: React.FC = ({ children }) => {
         }
       })
 
-    socket.private(`Company.${auth.user.company_id}.PartStock`)
+    const partStockChannel = socket
+      .private(`Company.${auth.user.company_id}.Stock`)
       .error((error: any) => {
         if (error.type === 'AuthError') {
           showErrorToast(
@@ -48,15 +54,23 @@ const PrivateLayout: React.FC = ({ children }) => {
           )
         }
       })
-      .listen('PartStockWarning', (ev: any) => {
-        showWarningToast(ev.message)
+
+    partStockChannel.listen('.PartStockWarning', ({ data }: any) => {
+      openDialog({
+        id: 'part-stock-warn',
+        stock: data.stock,
+        part: data.part,
+        partStockWarning: data.part_stock_warning,
+        partStock: data.part_stock,
       })
+    }
+    )
 
     return () => {
       socket.leave('User.' + auth?.user.id)
-      socket.leave(`Company.${auth.user.company_id}.PartStock`)
+      socket.leave(`Company.${auth.user.company_id}.Stock`)
     }
-  }, [auth])
+  }, [auth, socket])
 
   return (
     <div style={{ position: 'relative' }}>

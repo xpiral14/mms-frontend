1d45d6012ef8ff8e8d12fff8abf4096911c2d713
Samuel Reis <sa.reis1999@gmail.com>
2022-08-17 17:26:40 -0300

    feat: added more attributes

--- src/Contracts/Models/PartStock.ts
+++ src/Contracts/Models/PartStock.ts
@@ -5,4 +5,8 @@ export default interface PartStock {
   part_id: number;
   quantity: number;
   minimum: number;
+  part_name?: string
+  part_price?: number;
+  unit_id?: number;
+  unit_name?: string;
 }

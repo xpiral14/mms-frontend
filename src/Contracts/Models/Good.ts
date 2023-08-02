30c46d1325cdb5b63d6a4c2ff8c7e5192eaf041c
Samuel Araujo Reis <sa.reis1999@gmail.com>
2023-08-02 00:18:42 -0300

    feat: adicionado constantes

--- src/Contracts/Models/Good.ts
+++ src/Contracts/Models/Good.ts
@@ -1,11 +1,23 @@
 import GoodProduct from './GoodProduct'
 
+export enum GoodStatuses {
+  DISTRIBUTED = 'DISTRIBUTED',
+  NOT_DISTRIBUTED = 'NOT_DISTRIBUTED',
+  PARTIAL_DISTRIBUTED = 'PARTIAL_DISTRIBUTED'
+}
+
+export const GoodStatusesNames = {
+  [GoodStatuses.DISTRIBUTED]: 'Distribuído',
+  [GoodStatuses.NOT_DISTRIBUTED]: 'Não distribuído',
+  [GoodStatuses.PARTIAL_DISTRIBUTED]: 'Parcialmente distribuído',
+}
 export default interface Good {
   id?: number;
   invoice_number: string
   supplier_id: number
   received_at: string|Date
   distributed_at: string|Date|null
-  requested_at?: string|Date|null
-  good_products: GoodProduct[]
+  requested_at?: string | Date
+  goods_products: GoodProduct[]
+  status: GoodStatuses
 }

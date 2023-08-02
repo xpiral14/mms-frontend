672e485945d58ec98ad4cf1d6b96f37547aff9a9
Samuel Araujo Reis <sa.reis1999@gmail.com>
2023-08-02 00:19:36 -0300

    feat; adicionado contrato de tela

--- src/Contracts/Screen/AddProductToGood/index.tsx
+++ src/Contracts/Screen/AddProductToGood/index.tsx
@@ -1,8 +1,13 @@
 import ScreenProps from '../../Components/ScreenProps'
 import GoodProduct from '../../Models/GoodProduct'
 
-export interface AddProductToGoodProps{
-  onAddProduct: (goodProduct: Omit<Partial<GoodProduct>, 'good_id'>) => void
+export interface AddProductToGoodProps {
+  editMode?: boolean
+  goodProduct?: Partial<GoodProduct> | null
+  onAddProduct: (
+    goodProduct: Omit<Partial<GoodProduct>, 'good_id'>,
+    closeModal: () => void
+  ) => void
 } 
 export interface AddProductToGoodScreenProps extends ScreenProps, AddProductToGoodProps {
 }

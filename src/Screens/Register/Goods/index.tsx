ff1f80c73c9c4f042e571ef521736d0ffc091d75
Samuel Araujo Reis <sa.reis1999@gmail.com>
2023-08-02 00:25:05 -0300

    feat: adicionado logica de criacao de mercadoria

--- src/Screens/Register/Goods/index.tsx
+++ src/Screens/Register/Goods/index.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useMemo } from 'react'
+import React, { useMemo, useState, useCallback } from 'react'
 import RegistrationButtonBar from '../../../Components/RegistrationButtonBar'
 import InputText from '../../../Components/InputText'
 import PaginatedTable from '../../../Components/PaginatedTable'
@@ -8,16 +7,19 @@ import { StopLoadFunc } from '../../../Contracts/Components/RegistrationButtonBa
 import { useWindow } from '../../../Hooks/useWindow'
 import { useAlert } from '../../../Hooks/useAlert'
 import { ScreenStatus } from '../../../Constants/Enums'
-import { Intent } from '@blueprintjs/core'
+import { ButtonGroup, Intent } from '@blueprintjs/core'
 import { useToast } from '../../../Hooks/useToast'
 import useValidation from '../../../Hooks/useValidation'
 import { Validation } from '../../../Contracts/Hooks/useValidation'
-import Good from '../../../Contracts/Models/Good'
+import Good, {
+  GoodStatuses,
+  GoodStatusesNames,
+} from '../../../Contracts/Models/Good'
 import Render from '../../../Components/Render'
 import Container from '../../../Components/Layout/Container'
 import Row from '../../../Components/Layout/Row'
 import { GoodRegisterScreenProps } from '../../../Contracts/Screen/Register/Goods'
-import { Column } from '../../../Contracts/Components/Table'
+import { Column, Row as TableRow } from '../../../Contracts/Components/Table'
 import InputDate from '../../../Components/InputDate'
 import Table from '../../../Components/Table'
 import GoodProduct from '../../../Contracts/Models/GoodProduct'
@@ -27,17 +29,25 @@ import { useScreen } from '../../../Hooks/useScreen'
 import { AddProductToGoodProps } from '../../../Contracts/Screen/AddProductToGood'
 import currencyFormat from '../../../Util/currencyFormat'
 import useMessageError from '../../../Hooks/useMessageError'
+// import { DistributeGoodsProps } from '../../../Contracts/Screen/DistributeGoods'
+import Box from '../../../Components/Layout/Box'
 import { DistributeGoodsProps } from '../../../Contracts/Screen/DistributeGoods'
 
-const GoodsScreen: React.FC<GoodRegisterScreenProps> = (
-  {
-    screen,
-    supplierId,
-  },
-): JSX.Element => {
-  const { screenStatus, setScreenStatus, payload, setPayload, changePayloadAttribute } = useWindow<Good>()
+const GoodsScreen: React.FC<GoodRegisterScreenProps> = ({
+  screen,
+  supplierId,
+}): JSX.Element => {
+  const {
+    screenStatus,
+    setScreenStatus,
+    payload,
+    setPayload,
+    changePayloadAttribute,
+  } = useWindow<Good>()
   const { openSubScreen } = useScreen()
-
+  const [selectedGoodProduct, setSelectedProduct] =
+    useState<GoodProduct | null>(null)
+  const [loadingDeleteProduct, setLoadingDeleteProduct] = useState(false)
   const validations = [
     {
       errorMessage: 'O número da nota fiscal é obrigatória',
@@ -67,14 +77,20 @@ const GoodsScreen: React.FC<GoodRegisterScreenProps> = (
         },
         {
           name: 'Status de distribuição',
-          formatText: (row) => (row!.received_at ? 'Recebido' : 'Não recebido'),
+          formatText: (row: Good) => GoodStatusesNames[row.status],
         },
         {
           name: 'Valor total',
-          formatText: (row) => currencyFormat(row?.good_products?.reduce((t: number, c: GoodProduct) => t + c.value, 0) ?? 0),
+          formatText: (row) =>
+            currencyFormat(
+              row?.goods_products?.reduce(
+                (t: number, c: GoodProduct) => t + c.value,
+                0
+              ) ?? 0
+            ),
         },
       ] as Column[],
-    [],
+    []
   )
   const columns = useMemo(
     () =>
@@ -94,15 +110,42 @@ const GoodsScreen: React.FC<GoodRegisterScreenProps> = (
         {
           name: 'Valor unitário',
           formatText: (row) =>
-            currencyFormat((row!.value ?? 0) / (row!.quantity ?? 0)),
+            currencyFormat((row!.value ?? 0) / (row!.quantity ?? 1)),
         },
         {
           name: 'Valor total',
           formatText: (row) => currencyFormat(row?.value ?? 0),
         },
       ] as Column[],
-    [],
+    []
   )
+
+  const removeProduct = async () => {
+    openAlert({
+      text: 'Tem certeza que deseja remover o produto?',
+      intent: 'warning',
+      icon: 'warning-sign',
+      onConfirm: async () => {
+        try {
+          setLoadingDeleteProduct(true)
+          if(selectedGoodProduct!.id){
+            await GoodService.deleteGoodProduct(payload, selectedGoodProduct!)
+          }
+          setPayload((prev) => ({
+            ...prev,
+            goods_products: prev.goods_products?.filter(
+              (g) => g.product_id !== selectedGoodProduct?.product_id
+            ),
+          }))
+          setSelectedProduct(null)
+        } catch (error) {
+          showErrorMessage(error, 'Não foi possível excluir o produto. Por favor, tente novamente')
+        }finally{
+          setLoadingDeleteProduct(false)
+        }
+      },
+    })
+  }
   const handleAddProduct = () => {
     openSubScreen<AddProductToGoodProps>(
       {
@@ -110,27 +153,64 @@ const GoodsScreen: React.FC<GoodRegisterScreenProps> = (
       },
       screen.id,
       {
-        onAddProduct: (p) =>
-          setPayload((prev) => ({
-            ...prev,
-            good_products: [...(prev?.good_products || []), p as any],
-          })),
+        onAddProduct: (p) => {
+          setPayload((prev) => {
+            if (
+              prev.goods_products?.some((gp) => gp.product_id === p.product_id)
+            ) {
+              showWarningToast('O produto já foi adicionado a lista')
+              return { ...prev }
+            }
+            return {
+              ...prev,
+              goods_products: [...(prev?.goods_products || []), p as any],
+            }
+          })
+        },
+      }
+    )
+  }
+  const handleEditProduct = () => {
+    openSubScreen<AddProductToGoodProps>(
+      {
+        id: 'good-product-register',
       },
+      screen.id,
+      {
+        editMode: true,
+        goodProduct: selectedGoodProduct,
+        onAddProduct: (p, closeModal) => {
+          setPayload((prev) => {
+            const copy = { ...prev }
+            const goodProductIndex = prev.goods_products!.findIndex(
+              (gp) => gp.product_id === p.product_id
+            )
+            closeModal()
+            if (goodProductIndex !== -1) {
+              copy.goods_products![goodProductIndex] = p as any
+              return copy
+            }
+            return copy
+          })
+        },
+      }
     )
   }
 
-  const { showSuccessToast } = useToast()
+  const { showSuccessToast, showWarningToast } = useToast()
   const { showErrorMessage } = useMessageError()
   const { openAlert } = useAlert()
   const createGood = async (stopLoad: StopLoadFunc) => {
     try {
       const response = await GoodService.create({
+        status: GoodStatuses.NOT_DISTRIBUTED,
         supplier_id: supplierId,
         distributed_at: null,
         received_at: (payload.received_at as Date)?.toISOString(),
+        requested_at: (payload.requested_at as Date)?.toISOString(),
         invoice_number: payload.invoice_number!,
-        good_products:
-          (payload.good_products?.map((gp) => ({
+        goods_products:
+          (payload.goods_products?.map((gp) => ({
             product_id: gp.product_id,
             quantity: gp.quantity,
             value: gp.value,
@@ -149,16 +229,32 @@ const GoodsScreen: React.FC<GoodRegisterScreenProps> = (
     } catch (error: any) {
       showErrorMessage(
         error,
-        'Não foi possível fazer o cadastro do registro de mercadoria. Por favor, tente novamente',
+        'Não foi possível fazer o cadastro do registro de mercadoria. Por favor, tente novamente'
       )
     }
 
     stopLoad()
   }
 
-  const editGood = (stopLoad: StopLoadFunc) => {
-    stopLoad()
-    showSuccessToast('O registro de mercadoria foi atualizado com sucesso.')
+  const editGood = async (stopLoad: StopLoadFunc) => {
+    try {
+      await GoodService.update({
+        ...payload,
+        received_at: (payload.received_at as Date)?.toISOString(),
+        requested_at: (payload.requested_at as Date)?.toISOString(),
+        goods_products: payload.goods_products?.filter((gp) => !gp.id),
+      })
+      showSuccessToast('O registro de mercadoria foi atualizado com sucesso.')
+      setPayload({})
+      setScreenStatus(ScreenStatus.SEE_REGISTERS)
+    } catch (error) {
+      showErrorMessage(
+        error,
+        'Não foi possível atualizar o registro de mercadoria. Por favor, tente novamente'
+      )
+    } finally {
+      stopLoad()
+    }
   }
 
   const handleSaveButtonClick = (stopLoad: StopLoadFunc) => {
@@ -167,7 +263,7 @@ const GoodsScreen: React.FC<GoodRegisterScreenProps> = (
       return
     }
 
-    if (!payload.good_products?.length) {
+    if (!payload.goods_products?.length) {
       openAlert({
         text: 'Tem certeza que deseja cadastrar um registro de mercadorias que não possui mercadorias?',
         icon: 'warning-sign',
@@ -195,57 +291,81 @@ const GoodsScreen: React.FC<GoodRegisterScreenProps> = (
   }
 
   const handleDistributeGoodButtonClick = () => {
-    openSubScreen<DistributeGoodsProps>({
-      id: 'distribute-goods',
-    }, screen.id, {
-      good: payload,
+    if(!payload.goods_products?.length ){
+      showWarningToast('A mercadoria não possui produtos que possam ser distribuídos')
+      return
+    }
+    
+    openSubScreen<DistributeGoodsProps>(
+      {
+        id: 'distribute-goods',
+      },
+      screen.id,
+      {
+        good: payload,
+      }
+    )
+  }
+  const onRowSelect = useCallback((g: GoodProduct): void => {
+    setSelectedProduct(g)
+  }, [])
+  const isGoodProductSelect = useCallback(
+    (goodProduct: GoodProduct) =>
+      goodProduct.product_id === selectedGoodProduct?.product_id,
+    [selectedGoodProduct]
+  )
+  const onGoodSelect = (r: TableRow<Good>): void => {
+    return setPayload({
+      ...r,
+      received_at: r.received_at ? new Date(r.received_at) : null,
+      requested_at: r.requested_at ? new Date(r.requested_at as string) : null,
     })
   }
   return (
-    <Container style={{ height: 'calc(100% - 40px)' }}>
+    <Container style={{ height: 'calc(100% - 95px)' }}>
       <Row>
         <RegistrationButtonBar
           handleSaveButtonOnClick={handleSaveButtonClick}
+          handleDeleteButtonOnClick={async (stopLoad) => {
+            openAlert({
+              text: 'Tem certeza que deseja remover essa mercadoria?',
+              icon: 'warning-sign',
+              intent: Intent.DANGER,
+              onConfirm
+            })
+
+            async function onConfirm() {
+              try {
+
+                await GoodService.delete(payload)
+                showSuccessToast('Mercadoria excluída com sucesso')
+                setScreenStatus(ScreenStatus.SEE_REGISTERS)
+                setPayload({})
+              } catch (error) {
+                showErrorMessage(
+                  error,
+                  'Não foi possível excluir a mercadoria. Por favor, tente novamente'
+                )
+              } finally {
+                stopLoad()
+              }
+            }
+          }}
         />
       </Row>
       <Render renderIf={screenStatus === ScreenStatus.SEE_REGISTERS}>
         <Row className='my-2'>
           <Bar>
-            <Button intent={Intent.PRIMARY} onClick={handleDistributeGoodButtonClick} text='Distribuir mercadorias'
-              disabled={!payload.id || !!payload.distributed_at} />
+            <Button
+              intent={Intent.PRIMARY}
+              onClick={handleDistributeGoodButtonClick}
+              text='Distribuir mercadorias'
+              disabled={
+                !payload.id || payload.status === GoodStatuses.DISTRIBUTED
+              }
+            />
           </Bar>
         </Row>
-      </Render>
-      <Render renderIf={screenStatus !== ScreenStatus.SEE_REGISTERS}>
-        <Row>
-          <InputText
-            id={screen.createElementId('invoice-number')} value={payload.invoice_number ?? ''}
-            label='Número da nota fiscal'
-            onChange={(e) => changePayloadAttribute('invoice_number', e.target.value)} />
-          <InputDate
-            inputProps={{
-              style: { width: '100%' },
-            }}
-            timePrecision='minute'
-            label='Data de recebimento'
-            id={screen.id + 'received_at'}
-            onChange={d => changePayloadAttribute('received_at', d)}
-            value={payload.received_at as Date}
-          />
-        </Row>
-
-        <Row className='my-2'>
-          <Bar>
-            <Button icon='add' onClick={handleAddProduct} intent={Intent.PRIMARY}>
-              Adicionar produto
-            </Button>
-          </Bar>
-        </Row>
-
-        <Table rows={payload.good_products as any} columns={columns} />
-      </Render>
-
-      <Render renderIf={screenStatus === ScreenStatus.SEE_REGISTERS}>
         <Row className='h-100'>
           <PaginatedTable
             height='100%'
@@ -259,14 +379,84 @@ const GoodsScreen: React.FC<GoodRegisterScreenProps> = (
             }}
             columns={paginatedColumns}
             isSelected={(row: any) => row.id === payload?.id}
-            onRowSelect={(r) =>
-              setPayload({
-                ...r,
-                received_at: new Date(r.received_at),
-              })
-            }
+            onRowSelect={onGoodSelect}
           />
         </Row>
+      </Render>
+      <Render renderIf={screenStatus !== ScreenStatus.SEE_REGISTERS}>
+        <Box className='mt-2'>
+          <Row>
+            <InputText
+              id={screen.createElementId('invoice-number')}
+              value={payload.invoice_number ?? ''}
+              label='Número da nota fiscal'
+              onChange={(e) =>
+                changePayloadAttribute('invoice_number', e.target.value)
+              }
+            />
+            <InputDate
+              inputProps={{
+                style: { width: '100%' },
+              }}
+              timePrecision='minute'
+              label='Data do pedido'
+              id={screen.id + 'requested_at'}
+              onChange={(d) => changePayloadAttribute('requested_at', d)}
+              value={payload.requested_at as Date}
+            />
+            <InputDate
+              inputProps={{
+                style: { width: '100%' },
+              }}
+              timePrecision='minute'
+              label='Data de recebimento'
+              id={screen.id + 'received_at'}
+              onChange={(d) => changePayloadAttribute('received_at', d)}
+              value={payload.received_at as Date}
+            />
+          </Row>
+        </Box>
+
+        <Row className='my-2'>
+          <Bar>
+            <ButtonGroup>
+              <Button
+                icon='add'
+                onClick={handleAddProduct}
+                intent={Intent.PRIMARY}
+              >
+                Adicionar produto
+              </Button>
+              <Render renderIf={!payload.id}>
+                <Button
+                  icon='edit'
+                  onClick={handleEditProduct}
+                  text='Editar'
+                  disabled={
+                    !selectedGoodProduct || Boolean(selectedGoodProduct.id)
+                  }
+                />
+              </Render>
+
+              <Button
+                intent={Intent.DANGER}
+                icon='trash'
+                onClick={removeProduct}
+                loading={loadingDeleteProduct}
+                text='Remover'
+                disabled={!selectedGoodProduct}
+              />
+            </ButtonGroup>
+          </Bar>
+        </Row>
+
+        <Table<GoodProduct>
+          rows={payload.goods_products ?? []}
+          columns={columns}
+          onRowSelect={onRowSelect}
+          isSelected={isGoodProductSelect}
+          rowKey={(g) => g.product_id}
+        />
       </Render>
     </Container>
   )

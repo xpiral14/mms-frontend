516624dbf6b3f763851a3359f68e438e7d99e011
Samuel Araujo Reis <sa.reis1999@gmail.com>
2023-08-02 00:18:55 -0300

    feat: adicionado algumas propriedades

--- src/Contracts/Models/GoodProduct.ts
+++ src/Contracts/Models/GoodProduct.ts
@@ -1,13 +1,17 @@
+import DistributedGoodProduct from './DistributedGoodProduct'
 import Good from './Good'
+import GoodsDistribution from './GoodsDistribution'
 import Product from './Product'
 
 export default interface GoodProduct {
-  id?: number
+  id: number
   product_id: number
   good_id: number
   quantity: number
   value: number
-
+  has_distributed: boolean
   product: Partial<Product>
   good: Partial<Good>
+  distributedGoodProduct: DistributedGoodProduct[]
+  goods_distribution: GoodsDistribution[]
 }

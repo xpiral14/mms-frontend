61a4468e5715639f762f396de54ef51ad67c7d8b
xpiral14 <sa.reis1999@gmail.com>
2023-09-10 11:48:43 -0300

    feat: adicionado select assincrono

---
+++ src/Components/AsyncSelect/index.tsx
@@ -0,0 +1,52 @@
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import Select, { SelectProps } from '../Select'
+import { Option } from '../../Contracts/Components/Suggest'
+import { useToast } from '../../Hooks/useToast'
+import debounce from '../../Util/debounce'
+
+export type AsyncSearchFunction<T = any> = (
+  query: string | null
+) => Promise<Option<T>[]>
+interface AsyncSelectProps extends SelectProps {
+  searchFunction: AsyncSearchFunction
+}
+function AsyncSelect<T = any>({ searchFunction, ...props }: AsyncSelectProps) {
+  const [options, setOptions] = useState([] as Option[])
+  const [loading, setLoading] = useState(false)
+  const [query, setQuery] = useState<string | null>(null)
+  const { showErrorToast } = useToast()
+  const getOptions = useCallback(
+    async (query: string | null) => {
+      try {
+        setLoading(true)
+        setOptions(await searchFunction(query))
+        setQuery(query)
+      } catch (error) {
+        showErrorToast('Não foi possível obter os dados')
+      } finally {
+        setLoading(false)
+      }
+    },
+    [searchFunction]
+  )
+  const debounceSearchFunction = useMemo(
+    () =>
+      debounce(getOptions, 300),
+    [getOptions]
+  )
+  useEffect(() => {
+    debounceSearchFunction(null)
+  }, [debounceSearchFunction])
+
+  return (
+    <Select<T>
+      {...props}
+      onQueryChange={debounceSearchFunction}
+      items={options}
+      loading={loading}
+      handleButtonReloadClick={() => getOptions(query)}
+    />
+  )
+}
+
+export default AsyncSelect
